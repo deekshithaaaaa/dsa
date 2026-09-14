8af8b96f8fed6455444b2be97300da646fe98ee6
Deekshitha <deekshithanallamothu18@gmail.com>
2026-09-14 15:20:22 +0530

    DSA: practice string traversal, palindrome, frequency, and anagram problems

--- string.py
+++ string.py
@@ -138,3 +138,193 @@ if freq1==freq2:
 else:
       print("not")
 
+text = "programming"
+for i in text:
+    print(i)
+
+text = "programming"
+print(text[0])
+print(text[-1])
+print(text[4])
+
+text = "programming"
+print(text[:4])
+print(text[3:7])
+print(text[-3:])
+
+text = "programming"
+print(len(text))
+print(len(text)-1)
+
+text = "python"
+for i in range(len(text)):
+    print(i,text[i])
+
+first = "Deekshitha"
+last = "Nallamothu"
+print(first+" "+last)
+
+word = "Python"
+print(word*3)
+
+text = "programming"
+print("g" in text)
+print('z' in text)
+
+a = "python"
+b = "python"
+c = "Python"
+print(a==b)
+print(a==c)
+
+Input="programming"
+count=0
+for i in Input:
+    if i in 'aeiouAEIOU':
+        count+=1
+print(count)
+
+input='banana'
+count=0
+for i in input:
+    if i =='a':
+        count+=1
+print(count)
+
+text='banana'
+freq={}
+for i in text:
+    if i in freq:
+        freq[i]+=1
+    else:
+        freq[i]=1
+print(freq)
+
+text="banana"
+a=0
+for i in range(len(text)):
+    if text[i]=='a':
+        a=i
+print(a)
+
+text = "hello"
+vowels=0
+consonants=0
+for i in text:
+    if i in 'aeiouAEIOU':
+        vowels+=1
+    else:
+        consonants+=1
+print("Vowels:",vowels)
+print("Consonants:",consonants)
+
+text = "python"
+a=""
+for i in range(len(text)-1,-1,-1):
+    a=a+text[i]
+print(a)
+
+text = "madam"
+rev=text[::-1]
+if text==rev:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
+
+text = "level"
+a=""
+for i in range(len(text)-1,-1,-1):
+    a=a+text[i]
+if text==a:
+    print("palindrome")
+else:
+    print("not")
+    
+text = "hello world python"
+a=''
+for i in text:
+    if i==" ":
+        continue
+    else:
+        a=a+i
+print(a)
+
+text = "python12345"
+count=0
+for i in text:
+    if i in "1234567890":
+        count+=1
+print(count)
+
+text = "Python123@#"
+count=0
+for i in text:
+    if i.isalpha():
+        count+=1
+print(count)
+
+text = "PyThOn"
+uc=0
+lc=0
+for i in text:
+    if i.isupper():
+        uc+=1
+    else:
+        lc+=1
+print(uc)
+print(lc)
+
+text = "programming"
+a=""
+for i in text:
+    if i not in a:
+        a=a+i
+print(a)
+
+text = "aabbcdd"
+freq={}
+for i in text:
+    if i in freq:
+        freq[i]+=1
+    else:
+        freq[i]=1
+for j in freq:
+    if freq[j]==1:
+        print(j)
+        break
+
+text1 = "listen"
+text2 = "silent"
+freq1={}
+freq2={}
+for i in text1:
+    if i in freq1:
+        freq1[i]+=1
+    else:
+        freq1[i]=1
+for j in text2:
+    if j in freq2:
+        freq2[j]+=1
+    else:
+        freq2[j]=1
+print(freq1)
+print(freq2)
+if freq1==freq2:
+    print("Anagram")
+else:
+    print("not")
+
+text = "mississippi"
+freq={}
+for i in text:
+    if i in freq:
+        freq[i]+=1
+    else:
+        freq[i]=1
+max=0
+max_char=""
+for i in freq:
+    if freq[i]>max:
+        max=freq[i]
+        max_char=i
+print(max_char)
